fae4faf234966ec9e4279e269747566cb18f450e
Igor <58830706+igorslash@users.noreply.github.com>
2025-02-13 11:42:21 +0300

    Update llmObject.py

--- LlmObject/llmObject.py
+++ LlmObject/llmObject.py
@@ -1,27 +1,28 @@
 from pixellib.instance import instance_segmentation
 
-from Training.OOP.composition import person
-
-
-def object_detection_on_an_object():
+def object_detection_on_an_image():
     segment_image = instance_segmentation()
     segment_image.load_model("mask_rcnn_coco.h5")
-    target_class = segment_image.select_target_classes(person = True,
-                                                        car = True)
-    result =segment_image.segmentImage("C:\\Users\\User\PycharmProjects"
-                               "\Training\LlmObject\фото\i.webp",
-                               extract_segmented_objects=True,
-                               output_image_name="foto.jpg",
-                               show_bboxes=True,
-                               segment_target_classes=target_class,
-                               save_extracted_objects=True
-                               )
-    print(result[0])
-    objects = len(result[0])
-    print("Объектов на изображении:", objects)
+    
+    # Выбор целевых классов
+    target_classes = segment_image.select_target_classes(person=True, car=True)
+    
+    # Обработка изображения
+    result = segment_image.segmentImage(
+        image_path=r"C:\Users\User\PycharmProjects\Training\LlmObject\фото\i.webp",
+        show_bboxes=True,
+        segment_target_classes=target_classes,
+        output_image_name="foto.jpg",
+        extract_segmented_objects=True,
+        save_extracted_objects=True
+    )
+    
+    # Получение количества объектов
+    detected_objects = len(result[0]["class_ids"])
+    print(f"Обнаружено объектов: {detected_objects}")
 
 def main():
-    object_detection_on_an_object()
+    object_detection_on_an_image()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
